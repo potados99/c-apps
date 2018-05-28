c42cabbcb291dc520fa3690cf9efe98d5422fcd5
potados99 <potados99@gmail.com>
2018-05-28 23:12:24 +0900

    Remove comments

--- ioutil/ioutil.c
+++ ioutil/ioutil.c
@@ -21,37 +21,10 @@
 #include "ioutil.h"
 
 void _int_to_string(char *dest, const int num) {
-    /*
-     In: [num]
-     Out: [dest]
-     Description: convert integer to string
-     */
-    
     sprintf(dest, "%d", num);
 }
 
 void _int_to_string_with_comma(char *dest, const int num) {
-    /*
-     In: [num]
-     Out: [dest]
-     Description: convert integer to string with comma inserted
-     */
-    
-    /*
-     state table:
-     
-     1 2 , 3 4 5 , 6 7 8 (8 digits)
-     
-     val    1   2   3   4   5   6   7   8
-     index  0   1   2   3   4   5   6   7
-     i      0   1
-     j              2   3   4   5   6   7
-     comma? n   n   y   n   n   y
-     
-     if ((j % 3) == (strlen(buffer) % 3))
-        // insert comma before number
-     */
-    
     char buffer[_SMALL_BUFFER_SIZE];
     sprintf(buffer, "%d", num);
     
@@ -77,12 +50,6 @@ void _int_to_string_with_comma(char *dest, const int num) {
 }
 
 int _get_sum(const int *nums, const int count) {
-    /*
-     In: [num], [count]
-     Out: [sum]
-     Description: add every number in nums to sum
-     */
-    
     int sum = 0;
     for (unsigned register int i = 0; i < count; ++ i) {
         sum += nums[i];
@@ -92,13 +59,6 @@ int _get_sum(const int *nums, const int count) {
 
 
 char *get_input_string() {
-    /*
-     In: none
-     Out: [string]
-     Description: get a dynamic-allocated string from user input.
-     reject when it gets wrong input.
-     */
-    
     char buffer[_BUFFER_SIZE];
     rewind(stdin);
     fgets(buffer, _BUFFER_SIZE - 1, stdin);
@@ -119,13 +79,6 @@ char *get_input_string() {
 }
 
 int get_input_number() {
-    /*
-     In: none
-     Out: atoi(buffer)
-     Description: get a number from user input.
-     reject when it gets wrong input.
-     */
-    
     char buffer[_BUFFER_SIZE];
     rewind(stdin);
     fgets(buffer, _BUFFER_SIZE - 1, stdin);
@@ -142,33 +95,12 @@ int get_input_number() {
 }
 
 char *allocate_string(const char *buffer) {
-    /*
-     In:
-     [buffer]: array of char
-     Out:
-     [string]: pointer of allocated memmory
-     Description: allocate memmory for string
-     */
     char *string = (char *)malloc(sizeof(char) * strlen(buffer)+1);
     strcpy(string, buffer);
     return string;
 }
 
 void println_string_cells_with_token(const char **string, const int stringCount , const char token, const int *tokenLength, const char border) {
-    /*
-     In:
-     [string]: pointer of array of (char *) type string
-     [stringCount]: number of string
-     [token]: (char) type character. used to fill empty space
-     [tokenLengths]: pointer of array of (int) type number.
-     each element indicates the length of each cell
-     [border]: (char) type character. printed at head and tail.
-     Out: none
-     Description: print strings in horizontal-oriented cells which are divided by [border].
-     in each cell, string is center-aligned.
-     empty space in the cell is filled with [token]
-     */
-    
     for (int i = 0; i < stringCount; ++ i) {
         printf("%c", border);
         
@@ -190,18 +122,6 @@ void println_string_with_token(const char *string, const char token, const int t
 }
 
 void print_string_with_token(const char *string, const char token, const int tokenLength, const char border) {
-    /*
-     In:
-     [string]: pointer of (char *) type string
-     [token]: (char) type character. used to fill empty space
-     [tokenLength]: pointer of (int) type number. the length of cell.
-     [border]: (char) type character. printed at head and tail.
-     Out: none
-     Description: print string in a cell which is covered by [border].
-     in that cell, string is center-aligned.
-     empty space in the cell is filled with [token]
-     */
-    
     printf("%c", border);
     
     if (string) {
@@ -222,50 +142,19 @@ void println_token(const char token, const unsigned int length) {
 }
 
 void print_token(const char token, const unsigned int length) {
-    /*
-     In: [token], [length]
-     Out: none
-     Description: print [length] of tokens
-     */
-    
     for (unsigned register int i = 0; i < length; ++ i)
         printf("%c", token);
 }
 
 
 void wait_for_enter() {
-    /*
-     In: none
-     Out: none
-     Description: ask user for input until it gets '\n'
-     */
-    
     rewind(stdin);
     printf("\nPress enter.\n");
     while(getchar() != 10);/* CR */
     rewind(stdin);
 }
 
-void print_moving_string(const char *string, const int flowDirection, const int speed, const int boxLength, const char boxBorderLeft, const char boxBorderRight) {
-   /*
-    string: "Hello!"
-    !
-    o!
-    lo!
-    llo!
-    ello!
-    hello!
-     hello!
-      hello!
-        hello!
-         hello!
-          hello
-           hell
-            hel
-             he
-              h
-    */
-    
+void print_moving_string(const char *string, const int flowDirection, const int speed, const int boxLength, const char emptySpaceToken, const char boxBorderLeft, const char boxBorderRight) {
     for (register unsigned int frame = 0; frame < (boxLength + strlen(string)); ++ frame) {
         rewind(stdout);
         printf("\r");
@@ -283,7 +172,7 @@ void print_moving_string(const char *string, const int flowDirection, const int 
         if (flowDirection == STRING_FLOW_DIRECTION_RIGHT) {
             lastCharacterPosition = frame;
         } else {
-            // default: left
+            // default: flow_direction_left
             lastCharacterPosition = boxLength + (unsigned int)strlen(string) - frame - 1;
         }
         
@@ -304,13 +193,13 @@ void print_moving_string(const char *string, const int flowDirection, const int 
         }
         
         for (register unsigned int k = 0; k < beforSpaceLength; ++ k)
-            printf("%c", ' ');
+            printf("%c", emptySpaceToken);
         
         for (register unsigned int j = stringBegin; j < stringEnd; ++ j)
             printf("%c", string[j]);
         
         for (register unsigned int k = 0; k < afterSpaceLength; ++ k)
-            printf("%c", ' ');
+            printf("%c", emptySpaceToken);
         
         printf("%c", boxBorderRight);
     }
@@ -322,9 +211,3 @@ void print_moving_string(const char *string, const int flowDirection, const int 
     rewind(stdout);
     _sleep(1000 * 0.2);
 }
-
-//
-//  Author
-//  ID: 201701562
-//  Name: Byeong Jun Song
-//
